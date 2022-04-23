61b27852767e93359cdc8140774359a33b336b8f
patience-izere <peshobeat@gmail.com>
2022-04-23 13:53:54 -0700

    escaped answer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,52 @@
 #include "main.h"
 
-
-
 /**
  *
- * * infinite_add - C function that adds two numbers stored
+ *  * rev_string - reverse array
  *
- * *in strings to a buffer.
+ *   * @n: integer params
  *
- * *Assumes that strings are never empty and
+ *    * Return: 0
  *
- * *that numbers will always be positive, or 0.
- *
- * *Assumes there are only digits stored in the number strings.
- *
- * *If result can be stored in the buffer,
- *
- * *returns a pointer to the result.
- *
- * *If result cannot be stored in the buffer, returns `0`.
- *
- * *@n1:first number to be added
- *
- * *@n2:second number to be added
- *
- * *@r: store result
- *
- * *@size_r: size of buffer
- *
- * *Return:returns pointer to result
- *
- * */
+ *     */
 
 
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void rev_string(char *n)
 
 {
+
+		int i = 0;
+
+			int j = 0;
+
+				char temp;
+
+
+
+					while (*(n + i) != '\0')
+
+							{
+
+										i++;
+
+											}
+
+						i--;
+
+
+
+							for (j = 0; j < i; j++, i--)
+
+									{
+
+												temp = *(n + j);
+
+														*(n + j) = *(n + i);
+
+																*(n + i) = temp;
+
+																	}
 
 }
 
@@ -44,76 +54,100 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 /**
  *
- * * add_strings - Adds the numbers stored in two strings.
+ *  * infinite_add - add 2 numbers together
  *
- * * @n1: The string containing the first number to be added.
+ *   * @n1: text representation of 1st number to add
  *
- * * @n2: The string containing the second number to be added.
+ *    * @n2: text representation of 2nd number to add
  *
- * * @r: The buffer to store the result.
+ *     * @r: pointer to buffer
  *
- * * @r_index: The current index of the buffer.
+ *      * @size_r: buffer size
  *
- * *
+ *       * Return: pointer to calling function
  *
- * * Return: If r can store the sum - a pointer to the result.
- *
- * *         If r cannot store the sum - 0.
- *
- * */
+ *        */
 
 
 
-char *add_strings(char *n1, char *n2, char *r, int r_index)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 
-		int num, tens = 0;
+		int overflow = 0, i = 0, j = 0, digits = 0;
+
+			int val1 = 0, val2 = 0, temp_tot = 0;
 
 
 
-			for (; *n1 && *n2; n1--, n2--, r_index--)
+				while (*(n1 + i) != '\0')
 
-					{
+							i++;
 
-								num = (*n1 - '0') + (*n2 - '0');
+					while (*(n2 + j) != '\0')
 
-										num += tens;
+								j++;
 
-												*(r + r_index) = (num % 10) + '0';
+						i--;
 
-														tens = num / 10;
+							j--;
 
-															}
+								if (j >= size_r || i >= size_r)
 
+											return (0);
 
+									while (j >= 0 || i >= 0 || overflow == 1)
 
-				for (; *n1; n1--; r_index++)
+											{
 
-						{
+														if (i < 0)
 
-									num = *(n1 - '0') + tens; 
+																		val1 = 0;
 
-											*(r + r_index) = (num % 10) + '0';
+																else
 
-													tens = num / 10;
+																				val1 = *(n1 + i) - '0';
 
-														}
+																		if (j < 0)
 
+																						val2 = 0;
 
+																				else
 
-					for (; *n2; n2--;  r_index--)
+																								val2 = *(n2 + j) - '0';
 
-							{
+																						temp_tot = val1 + val2 + overflow;
 
-										num = (*n2 - '0') + tens; 
+																								if (temp_tot >= 10)
 
-												*(r + r_index) = (num % 10) + '0';
+																												overflow = 1;
 
-														tens = num / 10; 
+																										else
 
-															}
+																														overflow = 0;
 
-						
+																												if (digits >= (size_r - 1))
+
+																																return (0);
+
+																														*(r + digits) = (temp_tot % 10) + '0';
+
+																																digits++;
+
+																																		j--;
+
+																																				i--;
+
+																																					}
+
+										if (digits == size_r)
+
+													return (0);
+
+											*(r + digits) = '\0';
+
+												rev_string(r);
+
+													return (r);
 
 }
