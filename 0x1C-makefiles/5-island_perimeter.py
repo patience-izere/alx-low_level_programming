03e5187ae7699f8c562c041152d5c2283573ad2c
patience-izere <101501045+patience-izere@users.noreply.github.com>
2022-09-15 19:07:04 +0200

    it's gonna be fun

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module for island perimeter task """
+
+
+def island_perimeter(grid):
+    """ func to print perimeter of island """
+    water_zone = 0
+    land_zone = 1
+    cell_sidelen = 1
+    print("[], {}, {}".format(water_zone, land_zone, cell_sidelen))
--- 0x1C-makefiles/README.md
+++ 0x1C-makefiles/README.md
@@ -1,1 +1,5 @@
-my nameis pesho
+# 0x1C- Makefiles
+
+[Holberton School C Programming Projects](https://github.com/Jilroge7/holbertonschool-low_level_programming.git)
+
+## This folder contains the project "Makefies" and it's associated tasks:
